e852976aa5997a27ba55facf6fbc8b5d85962381
Alex <alexander.j.winter@outlook.com>
2026-02-22 00:30:28 +0100

    main.py simplified (path in working directory and in container always relative: "data")

--- main.py
+++ main.py
@@ -9,18 +9,20 @@ from retriever import get_retriever
 from prompt import get_prompt
 from rag_chain import build_rag_chain
 from query import ask_question
-from pathlib import Path
 
 def main():
-    PDF_DIR = Path("/data") if Path("/data").exists() else Path("data")
+    PDF_DIR = "data"
     print(PDF_DIR)
+
     docs = load_pdf(PDF_DIR)
     split_docs = split_documents(docs, CHUNK_SIZE, CHUNK_OVERLAP)
     embeddings = get_embeddings()
     vectorstore = build_vectorstore(split_docs, embeddings, CHROMA_DIR)
     retriever = get_retriever(vectorstore)
     prompt = get_prompt()
+
     llm = ChatOpenAI(api_key=OPENAI_TOKEN, model="gpt-4")
+
     rag_chain = build_rag_chain(retriever, prompt, llm)
     ask_question(rag_chain)
 
